4f3f3b13b4eb7c3f710699efeb54106a4934f3f5
Aravinth VARATHARAJAH <AVARATHARAJAH@micropole.com>
2023-01-16 10:52:53 +0100

    dev: added static server

--- Back/server.js
+++ Back/server.js
@@ -1,1 +1,21 @@
-console.log("Hello !"); 
+console.log('Hello !');
+
+const express = require('express');
+const serveIndex = require('serve-index');
+
+const app = express();
+const port = 3000;
+
+// use pour tous les verbes
+
+app.use('/', (req, res, next) => {
+  console.log('req: ', req.url);
+  next();
+});
+
+app.use(express.static('.'));
+app.use(serveIndex('.', { icons: true }));
+
+app.listen(port, () => {
+  console.log(`Example app listing on port ${port}`);
+});
