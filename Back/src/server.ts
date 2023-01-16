14853766e139fbb6c15349f443cbb6a81621ddab
Aravinth VARATHARAJAH <AVARATHARAJAH@micropole.com>
2023-01-16 11:46:32 +0100

    dev: added typings

--- Back/src/server.ts
+++ Back/src/server.ts
@@ -1,7 +1,7 @@
 console.log('Hello !');
 
-const express = require('express');
-const serveIndex = require('serve-index');
+import express from "express";
+import serveIndex from 'serve-index';
 
 const app = express();
 const port = 3000;
