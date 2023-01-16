9d80fccf2b7222bfe66d709f42da00404a83e469
Aravinth VARATHARAJAH <AVARATHARAJAH@micropole.com>
2023-01-16 15:24:25 +0100

    dev: created new project with vitejs

---
+++ Front/src/main.ts
@@ -0,0 +1,28 @@
+import './style.css';
+
+const svgns = 'http://www.w3.org/2000/svg';
+
+const container = document.querySelector('g.samples');
+
+const r = 1;
+const r0 = 45;
+const cx0 = 50;
+const cy0 = 50;
+
+const getAnglesFormIndex = (index: number) => (index * 2 * Math.PI) / 10;
+const getPointsFormAngles = (angle: number) => {
+  return {
+    x: cx0 + r0 * Math.cos(angle),
+    y: cy0 + r0 * Math.sin(angle),
+  };
+};
+
+for (let i = 0; i < 10; i++) {
+  const angle = getAnglesFormIndex(i);
+  const { x: cx, y: cy } = getPointsFormAngles(angle);
+  const circle = document.createElementNS(svgns, 'circle');
+  circle.setAttributeNS(null, 'cx', cx + '');
+  circle.setAttributeNS(null, 'cy', cy + '');
+  circle.setAttributeNS(null, 'r', r + '');
+  container && container.appendChild(circle);
+}
